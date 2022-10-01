46e87a0922d5cc80b64a0bd9a64875e048bfc9b9
rosnerdev <yrosner123@gmail.com>
2022-10-01 12:19:31 +0300

    first commit

---
+++ src/Component.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { getContext } from "svelte"
+  import katex from "katex";
+	export let math = "x_1=\\frac{-b+\\sqrt{b^2-4ac}}{2a}";
+	export let displayMode = false;
+	
+	const options = {
+		displayMode: displayMode,
+		throwOnError: false
+	}
+	
+	$: katexString = katex.renderToString(math, options);
+
+  const { styleable } = getContext("sdk")
+  const component = getContext("component")
+</script>
+
+<div use:styleable={$component.styles}>
+  {@html katexString}
+</div>
+
+<svelte:head>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css" integrity="sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X" crossorigin="anonymous">
+</svelte:head>
